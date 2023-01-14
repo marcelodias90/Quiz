6a054c5c55795016572b6041c7145fb1479880f9
Marcelo Dias <diasteixeira1990@gmail.com>
2023-01-14 10:59:18 -0300

    novas questões, resultado, resposta

--- pages/resultado.tsx
+++ pages/resultado.tsx
@@ -1,22 +1,24 @@
 import styles from '../styles/Resultado.module.css'
 import { useRouter } from "next/router"
 import Estatistica from '../components/Estatistica'
+import Botao from '../components/Botao'
 
-export default function Resultado(){
+export default function Resultado() {
     const router = useRouter()
 
     const total = +router.query.total
     const certas = +router.query.certas
     const percentual = Math.round((certas / total) * 100)
 
-    return(
+    return (
         <div className={styles.resultado}>
             <h1>Resultado Final</h1>
-            <div style={{display: "flex"}}>
-            <Estatistica texto="Perguntas" valor={total}/>
-            <Estatistica texto="Certas" valor={certas} corFundo="#9CD2A4"/>
-            <Estatistica texto="Percentual" corFundo='#DE6A33' valor={`${percentual}%`}/>
+            <div style={{ display: "flex" }}>
+                <Estatistica texto="Perguntas" valor={total} />
+                <Estatistica texto="Certas" valor={certas} corFundo="#9CD2A4" />
+                <Estatistica texto="Percentual" corFundo='#DE6A33' valor={`${percentual}%`} />
             </div>
+            <Botao href='/' texto='Tentar Novamente' />
         </div>
     )
 }
